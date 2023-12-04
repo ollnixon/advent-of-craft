6497e1f50a098b053e8f1019000c6bb0829ccf39
Oliver Nixon <oliver@moogi.co.uk>
2023-12-04 12:29:49 +0000

    Extract isDivisibleByX methods

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -10,16 +10,28 @@ public class FizzBuzz {
 
         if (input > 100) throw new OutOfRangeException();
 
-        if (input % 3 == 0 && input % 5 == 0) {
+        if (isDivisibleBy3and5(input)) {
             return "FizzBuzz";
         }
-        if (input % 3 == 0) {
+        if (isDivisibleBy3(input)) {
             return "Fizz";
         }
-        if (input % 5 == 0) {
+        if (isDivisibleBy5(input)) {
             return "Buzz";
         }
 
         return input.toString();
+    }
+
+    private static boolean isDivisibleBy5(Integer input) {
+        return input % 5 == 0;
+    }
+
+    private static boolean isDivisibleBy3(Integer input) {
+        return input % 3 == 0;
+    }
+
+    private static boolean isDivisibleBy3and5(Integer input) {
+        return isDivisibleBy3(input) && isDivisibleBy5(input);
     }
 }
